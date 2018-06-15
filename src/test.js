19617dc83c36dd376d7a34fd6f33eae5131fe417
Rion Park <redfila@nate.com>
2018-06-16 00:37:16 +0900

    init2

---
+++ src/test.js
@@ -0,0 +1,6 @@
+let a = 3;
+console.log(a);
+let b = 5;
+console.log(b);
+let c = 30;
+//# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,7 @@
+
 let a = 3;
 console.log(a);
+let b =5;
+console.log(b);
+
+let c:number = 30;
